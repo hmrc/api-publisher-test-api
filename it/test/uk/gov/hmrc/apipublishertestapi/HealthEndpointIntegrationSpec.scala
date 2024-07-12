74f170a1a5e68df110a20488b4139ba31ffa67c6
John Green <john.sg.pub@gmail.com>
2024-07-12 16:06:57 +0100

    APIS-7194: Upgrade to Java 21 (#2)
    * APIS-7194: Upgrade to Java 21
    
    * APIS-7194: PR bot changes

--- it/test/uk/gov/hmrc/apipublishertestapi/HealthEndpointIntegrationSpec.scala
+++ it/test/uk/gov/hmrc/apipublishertestapi/HealthEndpointIntegrationSpec.scala
@@ -20,16 +20,17 @@ import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
+
 import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.ws.WSClient
 
 class HealthEndpointIntegrationSpec
-  extends AnyWordSpec
-     with Matchers
-     with ScalaFutures
-     with IntegrationPatience
-     with GuiceOneServerPerSuite {
+    extends AnyWordSpec
+    with Matchers
+    with ScalaFutures
+    with IntegrationPatience
+    with GuiceOneServerPerSuite {
 
   private val wsClient = app.injector.instanceOf[WSClient]
   private val baseUrl  = s"http://localhost:$port"
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
-  private val bootstrapVersion = "8.1.0"
+  private val bootstrapVersion = "9.0.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
